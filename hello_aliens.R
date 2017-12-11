29a6944b06751c9b03ec3b7fe5cf30e0104d30ce
Ina Rastegar <irastega@andrew.cmu.edu>
2017-12-10 20:55:51 -0500

    committed

---
+++ hello_aliens.R
@@ -0,0 +1,6 @@
+Hello Aliens!
+  Welcome to a quick info session on Planet Earth. 
+home to many species
+creator of the term "intelligent life"
+guilty of stereotyping aliens
+Many have dedicated their lives to understanding yours so we hope to make a hospitable place for you. 
